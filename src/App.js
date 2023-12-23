2202a9b69f882171f8b71c081c4a3f24b596a026
Satyam Pund <pundsatyam@gmail.com>
2023-12-24 01:44:08 +0530

    useReducer setup for quiz

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import React from 'react';
-import DateCounter from './DateCounter';
+import React, { useEffect, useReducer } from 'react';
+import Header from './Header.js';
+import Main from './Main.js';
+
+const initialState = {
+  questions: [],
+  //loading, error, ready, active, finished,
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready',
+      };
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error',
+      };
+    default:
+      throw new Error('Unknown error');
+  }
+}
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    fetch('http://localhost:9000/questions')
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch((err) => dispatch({ type: 'dataFailed' }));
+  }, []);
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Questions?</p>
+      </Main>
     </div>
   );
 };
